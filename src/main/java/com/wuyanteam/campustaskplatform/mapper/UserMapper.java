6ad2731c2d0786379bdff97a6564009c3a6d1d2f
5380ez <2375630142@qq.com>
2024-08-21 09:32:16 +0800

    Update UserMapper.java
    Co-Authored-By: Zzz0101011 <169972665+zzz0101011@users.noreply.github.com>

--- src/main/java/com/wuyanteam/campustaskplatform/mapper/UserMapper.java
+++ src/main/java/com/wuyanteam/campustaskplatform/mapper/UserMapper.java
@@ -1,10 +1,32 @@
 package com.wuyanteam.campustaskplatform.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wuyanteam.campustaskplatform.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper extends BaseMapper<User>
 {
+    @Select("SELECT\n" +
+            "task.take_time,\n" +
+            "task.state,\n" +
+            "task.reward,\n" +
+            "task.due_time,\n" +
+            "task.title,\n" +
+            "`user`.username\n" +
+            "FROM\n" +
+            "task\n" +
+            "INNER JOIN `user` ON task.publisher_id = `user`.id\n" +
+            "WHERE\n" +
+            "task.taker_id\n" +
+            "ORDER BY\n" +
+            "task.take_time ASC\n")
+    @Results(
+            {
+                @Result(column = )
+            }
+    )
 
 }
